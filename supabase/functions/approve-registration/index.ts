0d91af7b8dac5b995a17b7b7d272a4afd015832f
rasya19 <ismantoj88@gmail.com>
2026-06-20 03:10:00 +0700

    Update index.ts

--- supabase/functions/approve-registration/index.ts
+++ supabase/functions/approve-registration/index.ts
@@ -1,4 +1,4 @@
-// supabase/functions/approve-registration/index.ts
+// trigger deploy
 import { serve } from "https://deno.land/std@0.168.0/http/server.ts";
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 import { Resend } from "https://esm.sh/resend@2";
